539faf441963ad3017d11552287a09bab4d0f613
Bhumi Prasad <prasad14bhumi@gmail.com>
2025-10-26 13:40:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Bot from './components/Bot'
+const App = () => {
+  return (
+    <div className="bg-[url('/bg_img.webp')] bg-cover bg-center bg-black/80 bg-blend-overlay">
+      <Bot/>
+    </div>
+  )
+}
+
+export default App
